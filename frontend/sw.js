ef45ebc1f6cdaecd3a48cf1b78880d2a92921730
Abeel87 <gipgibon@gmail.com>
2026-08-20 06:14:34 +0000

    Tenis AI v6 new UI and community

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const C='tenis-ai-v53';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.webmanifest'])))});
+const C='tenis-ai-v60';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(C).then(c=>c.addAll(['./','index.html','style.css','app.js','manifest.webmanifest','icon-192.png','icon-512.png']))) });
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
